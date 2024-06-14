c815788b5e89f39baafada8948f2fbef53ae44f0
RajatAgrawal <rag7731@gmail.com>
2024-06-14 16:13:35 +0530

    again

--- db/dbcon.js
+++ db/dbcon.js
@@ -1,5 +1,5 @@
 const mongoose  = require('mongoose')
-const liveUrl = 'mongodb+srv://collablab2243:Sustainiser@cluster0.ddmucj4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const liveUrl = 'mongodb+srv://collablab2243:Sustainiser@cluster0.ddmucj4.mongodb.net/Sustainiser?retryWrites=true&w=majority&appName=Cluster0'
 const localUrl = 'mongodb://127.0.0.1:27017/sustainiser'
 
 const connectDb = () => {
