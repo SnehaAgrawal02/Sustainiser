18083119b726f3942831655287b24afc35ca38a0
RajatAgrawal <rag7731@gmail.com>
2024-06-14 16:12:00 +0530

    live mongodb

--- db/dbcon.js
+++ db/dbcon.js
@@ -1,5 +1,5 @@
 const mongoose  = require('mongoose')
-const liveUrl = 'mongodb+srv://rag7731:Rajat123@cluster0.5tva6b3.mongodb.net/AdmissionPortal?retryWrites=true&w=majority&appName=Cluster0'
+const liveUrl = 'mongodb+srv://collablab2243:Sustainiser@cluster0.ddmucj4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const localUrl = 'mongodb://127.0.0.1:27017/sustainiser'
 
 const connectDb = () => {
