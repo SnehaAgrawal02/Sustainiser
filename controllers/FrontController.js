732861f3e690eee3dd419c47bc78e26e72b76140
sneha <snehaagrawal0204@gmail.com>
2024-06-15 19:26:45 +0530

    narratives

--- controllers/FrontController.js
+++ controllers/FrontController.js
@@ -130,6 +130,16 @@ class FrontController {
         }
     }
 
+    //narratives
+    static narratives = async (req, res) => {
+        try{
+
+            res.render('narratives');
+        }catch(err){
+            console.log(err);
+        }
+    }
+
     static userinsert = async (req, res) => {
         try {
             let imageUpload = null; // Set default value for image upload
